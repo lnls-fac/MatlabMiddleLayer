b6088811f7553cab607ed0936589c34892165073
Daniel Tavares <daniel.tavares@lnls.br>
2015-04-16 09:55:50 -0300

    Cosmetic changes

--- Release/lnls/dig_scripts/uvx/fofb/fcexpproc/fcexpprocmap.m
+++ Release/lnls/dig_scripts/uvx/fofb/fcexpproc/fcexpprocmap.m
@@ -2,20 +2,25 @@ function expmap = fcexpprocmap(filenames)
 
 filenpts = 100e3;
 
+fprintf('\nLoading markers...');
 data = faload(filenames,0,0);
 t = find(data.marker(2:end) ~= data.marker(1:end-1));
 
+fprintf('\nMapping experiments...');
 j=1;
 for i=1:length(t)-1
     idx = t(i)+1:t(i+1);
     if data.marker(idx(1)) ~= 0
+        expmap(j).marker = data.marker(idx(1));
+
         filesidx = (floor(idx(1)/filenpts)+1):ceil(idx(end)/filenpts);
-        fprintf('%d:%d\n',filesidx(1),filesidx(end));
         idx = idx - (filesidx(1)-1)*filenpts;
-        
+
         expmap(j).idx = idx;
         expmap(j).filesidx = filesidx;
-        expmap(j).marker = data.marker;
+        
         j=j+1;
     end
 end
+
+fprintf('\n%4d files analyzed.\n%4d experiments found.\n',length(filenames),j-1);
--- Release/lnls/dig_scripts/uvx/fofb/fcexpproc/fcexpprocsave.m
+++ Release/lnls/dig_scripts/uvx/fofb/fcexpproc/fcexpprocsave.m
@@ -1,9 +1,15 @@
 function fcexpprocsave(filenames, expmap)
 
+fprintf('\nSaving experiments to file...');
 for i=1:length(expmap)
     data = faload(filenames(expmap(i).filesidx));
     data = facutdata(data, expmap(i).idx);
     timestamp = datestr(fatimelvrt2m(data.time(1)), 'yyyy.mm.dd_HH.MM.ss');
     
     save(sprintf('exp_%s.mat',timestamp),'data');
+	
+    if ~mod(i-1,50), fprintf('\n%4d ',i); end
+    fprintf('.');
 end
+
+fprintf('\n%d experiments saved.\n',i);
