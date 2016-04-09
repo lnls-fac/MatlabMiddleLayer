bd88e6e0b6c7abbc493fd5cdfaa7e992d7b932f5
Fernando <fernandohds564@gmail.com>
2016-04-08 21:38:36 -0300

    Creation of SI.V15.01

---
+++ Release/machine/SIRIUS/SI.V15.01/opt_results/s10/s10_01.m
@@ -0,0 +1,47 @@
+
+%  QUADRUPOLOS
+%  ===========
+qfa_strength  = 3.564163099141;
+qda_strength  =-1.537784298689;
+qdb2_strength =-3.204319620651;
+qfb_strength  = 4.096109116982;
+qdb1_strength =-2.057582400574;
+qdp2_strength = 0.000000000000;
+qfp_strength  = 2.305971093805;
+qdp1_strength =-1.407829594769;
+q1_strength   = 2.923551830042;
+q2_strength   = 4.265188804423;
+q3_strength   = 3.273391092836;
+q4_strength   = 3.937386169778;
+
+% same sextupoles as si.v14.c03
+%  SEXTUPOLOS
+%  ===========
+sda0_strength   = -79.9696;
+sdba0_strength  = -59.7268;
+sdbp0_strength  = -59.7268;
+sdp0_strength   = -79.9696;
+sda1_strength   =-168.4648;
+sdba1_strength  =-129.9407;
+sdbp1_strength  =-129.9407;
+sdp1_strength   =-168.4648;
+sda2_strength   = -92.0401;
+sdba2_strength  =-126.6479;
+sdbp2_strength  =-126.6479;
+sdp2_strength   = -92.0401;
+sda3_strength   =-123.8271;
+sdba3_strength  =-182.9644;
+sdbp3_strength  =-182.9644;
+sdp3_strength   =-123.8271;
+sfa0_strength   =  54.3899;
+sfba0_strength  =  73.6649;
+sfbp0_strength  =  73.6649;
+sfp0_strength   =  54.3899;
+sfa1_strength   = 191.9406;
+sfba1_strength  = 228.3979;
+sfbp1_strength  = 228.3979;
+sfp1_strength   = 191.9406;
+sfa2_strength   = 154.7264;
+sfba2_strength  = 213.3971;
+sfbp2_strength  = 213.3971;
+sfp2_strength   = 154.7264;
