b2f0bf2934943fec2e1b2990d9de92b2504a6718
ximenes <xresende@gmail.com>
2016-04-30 21:28:09 -0300

    Update in lnls_turn_sextupoles_off
    - so that it does not require MML data structure.

--- Release/applications/lnls/lnls_turn_sextupoles_off.m
+++ Release/applications/lnls/lnls_turn_sextupoles_off.m
@@ -1,8 +1,16 @@
 function the_ring = lnls_turn_sextupoles_off(the_ring0)
 
 the_ring = the_ring0;
-sextupoles = findmemberof('sext');
-for i=1:length(sextupoles)
-    idx = findcells(the_ring, 'FamName', sextupoles{i});
-    the_ring = setcellstruct(the_ring, 'PolynomB', idx, 0, 1, 3);
-end
+
+% MML-independent code
+bends = findcells(the_ring, 'BendingAngle');
+quads = setdiff(findcells(the_ring, 'PolynomB'), bends);
+sexts = quads(getcellstruct(the_ring, 'PolynomB', quads, 1, 3) ~= 0);
+the_ring = setcellstruct(the_ring, 'PolynomB', sexts, 0, 1, 3);
+
+% MML-dependent code
+% sextupoles = findmemberof('sext');
+% for i=1:length(sextupoles)
+%     idx = findcells(the_ring, 'FamName', sextupoles{i});
+%     the_ring = setcellstruct(the_ring, 'PolynomB', idx, 0, 1, 3);
+% end
