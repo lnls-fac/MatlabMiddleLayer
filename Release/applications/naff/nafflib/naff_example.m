cf4e01c04fc902d9d36901a71324cf69e4ddeaa3
Fernando <fernandohds564@gmail.com>
2016-08-01 10:13:26 -0300

    Bug correction in naff. Variable KTABS was being misinterpreted.

--- Release/applications/naff/nafflib/naff_example.m
+++ Release/applications/naff/nafflib/naff_example.m
@@ -1,9 +1,9 @@
 function naff_example
 % Example to test naff within matlab
 
-NT = 9996; % divided by 6
+NT = 9997; % NT-1 is divisible by 6
 
-% simple quasiperiodic (even period) motion 
+% simple quasiperiodic (even period) motion
 y =2+0.1*cos(pi*(0:NT-1))+0.00125*cos(pi/3*(0:NT-1));
 yp=2+0.1*sin(pi*(0:NT-1))+0.00125*sin(pi/3*(0:NT-1));
 
