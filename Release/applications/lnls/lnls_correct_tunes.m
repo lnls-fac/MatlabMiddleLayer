256464276e612b8bcadba5fb6318da51655f7dd8
Fernando <fernandohds564@gmail.com>
2016-09-06 20:28:09 -0300

    changing optics matching with IDs.
    Now there is only a local symmetrization with global tune and chromaticity correction.

--- Release/applications/lnls/lnls_correct_tunes.m
+++ Release/applications/lnls/lnls_correct_tunes.m
@@ -1,12 +1,16 @@
-function [the_ring, converged, tunesf, tunesi] = lnls_correct_tunes(the_ring, families, goal_tunes, max_iter, tolerancia)
-% [the_ring0, converged, tunes] = lnls_correct_tune(the_ring, families, goal_tune, max_iter, tolerancia)
-% 
+function [the_ring, converged, tunesf, tunesi] = lnls_correct_tunes(the_ring, goal_tunes, families, method, max_iter, tolerancia)
+% [the_ring0, converged, tunes] = lnls_correct_tune(the_ring, goal_tunes, families, method, max_iter, tolerancia)
+%
 % Correct tunes with specified quadrupole families.
 % Inputs:
 %   the_ring   : model of the ring;
 %   families   : cell of strings with the family names of the quadrupoles
-%                 to be used;
+%                to be used;
 %   goal_tunes : goal tunes, including the integer;
+%   method     : if 'svd' uses svd considering each family as a knob, else
+%                groups focusing and defocusing (by family name) in two
+%                knobs and apply variations proportional to the strength of
+%                each magnet.
 %   max_iter   : maximum # of trials for the correction algorithm;
 %   tolerancia : acceptable euclidian distance between tunes0 and goal_tunes
 %                 for the definition of convergence of the algorithm;
@@ -18,6 +22,14 @@ function [the_ring, converged, tunesf, tunesi] = lnls_correct_tunes(the_ring, fa
 %   tunesf     : tunes of the model the_ring0;
 %   tunesi     : initial tunes, before adjust.
 
+if ~exist('method', 'var') || isempty(method)
+    method = 'svd';
+end
+
+if ~exist('families', 'var') || isempty(families)
+    families = {'qda', 'qfa', 'qdb1', 'qdb2', 'qfb', 'qdp1', 'qdp2', 'qfp'};
+end
+
 if ~exist('max_iter', 'var')
     max_iter = 10;
 end
@@ -26,57 +38,121 @@ if ~exist('tolerancia', 'var')
     tolerancia = 1e-6;
 end
 
-% cria lista de indices a elementos do modelo AT
-knobs = cell(1,length(families));
-for ii=1:length(families)
-    knobs{ii}  = findcells(the_ring, 'FamName', families{ii});
-end
-
-calc_rms = @(x)norm(x)/sqrt(length(x));
-
-[~, tunes] = twissring(the_ring,0,1:length(the_ring)+1);
-tunesf = tunes;
-tunesi = tunes;
-converged = false;
-[pseudoinv_mm, abort] = calc_tune_matrix(the_ring, knobs);
-if abort, return; end
-factor = 1;
-res_vec = (tunes-goal_tunes)';
-res = calc_rms(res_vec);
-ii = 0;
-while (res > tolerancia) && (ii <max_iter) && (factor > 0.001)
-    dK = - factor * pseudoinv_mm * res_vec;
-    
-    new_the_ring = the_ring;
-    for i=1:length(knobs)
-        K = getcellstruct(new_the_ring, 'PolynomB', knobs{i},1,2);
-        new_the_ring = setcellstruct(new_the_ring, 'PolynomB',knobs{i}, K + dK(i), 1, 2);
+if strcmpi(method,'svd');
+    % cria lista de indices a elementos do modelo AT
+    knobs = cell(1,length(families));
+    for ii=1:length(families)
+        knobs{ii}  = findcells(the_ring, 'FamName', families{ii});
     end
     
-    [~, tunes] = twissring(new_the_ring,0,1:length(new_the_ring)+1);
-    new_res_vec = (tunes-goal_tunes)';
-    new_res = calc_rms(new_res_vec);
+    calc_rms = @(x)norm(x)/sqrt(length(x));
     
-    if (new_res < res)
-        [pseudoinv_mm, abort] = calc_tune_matrix(new_the_ring, knobs);
-        if abort, break; end
-        res_vec = new_res_vec;
-        res = new_res;
-        the_ring = new_the_ring;
-        tunesf = tunes;
-        factor = min([factor * 2, 1]);
-    else
-        factor = factor / 2;
-%         [pseudoinv_mm, abort] = calc_tune_matrix(the_ring, knobs);
-%         if abort, break; end
+    [~, tunes] = twissring(the_ring,0,1:length(the_ring)+1);
+    tunesf = tunes;
+    tunesi = tunes;
+    converged = false;
+    [pseudoinv_mm, abort] = calc_tune_matrix_svd(the_ring, knobs);
+    if abort, return; end
+    factor = 1;
+    res_vec = (tunes-goal_tunes)';
+    res = calc_rms(res_vec);
+    ii = 0;
+    while (res > tolerancia) && (ii <max_iter) && (factor > 0.001)
+        dK = - factor * pseudoinv_mm * res_vec;
+        
+        new_the_ring = the_ring;
+        for i=1:length(knobs)
+            K = getcellstruct(new_the_ring, 'PolynomB', knobs{i},1,2);
+            new_the_ring = setcellstruct(new_the_ring, 'PolynomB',knobs{i}, K + dK(i), 1, 2);
+        end
+        
+        [~, tunes] = twissring(new_the_ring,0,1:length(new_the_ring)+1);
+        new_res_vec = (tunes-goal_tunes)';
+        new_res = calc_rms(new_res_vec);
+        
+        if (new_res < res)
+            [pseudoinv_mm, abort] = calc_tune_matrix_svd(new_the_ring, knobs);
+            if abort, break; end
+            res_vec = new_res_vec;
+            res = new_res;
+            the_ring = new_the_ring;
+            tunesf = tunes;
+            factor = min([factor * 2, 1]);
+        else
+            factor = factor / 2;
+            %         [pseudoinv_mm, abort] = calc_tune_matrix(the_ring, knobs);
+            %         if abort, break; end
+        end
+        ii = ii + 1;
     end
-    ii = ii + 1;
+    
+    converged = res < tolerancia;
+else
+    % cria lista de indices de elementos do modelo AT
+    kn = [];
+    kp = [];
+    for ii=1:length(families)
+        if strncmpi(families{ii},'qd',2)
+            kn  = [kn, findcells(the_ring, 'FamName', families{ii})];
+        elseif strncmpi(families{ii},'qf',2)
+            kp  = [kp, findcells(the_ring, 'FamName', families{ii})];
+        else
+            fprintf('There is no way to know if family %s is focusing or defocusing\n',families{ii});
+            return;
+        end
+    end
+    if isempty(kp), fprintf('No focusing families found\n');
+    elseif isempty(kn), fprintf('No defocusing families found\n');end
+    
+    kn = sort(kn); kp = sort(kp);
+    
+    calc_rms = @(x)norm(x)/sqrt(length(x));
+    
+    [~, tunes] = twissring(the_ring,0,1:length(the_ring)+1);
+    tunesf = tunes;
+    tunesi = tunes;
+    converged = false;
+    [mm, abort] = calc_tune_matrix(the_ring, kn, kp);
+    if abort, return; end
+    factor = 1;
+    res_vec = (tunes-goal_tunes);
+    res = calc_rms(res_vec);
+    ii = 0;
+    while (res > tolerancia) && (ii <max_iter) && (factor > 0.001)
+        dK = -factor * (res_vec/ mm) ;
+        
+        new_the_ring = the_ring;
+        K = getcellstruct(new_the_ring, 'PolynomB', kn,1,2);
+        new_the_ring = setcellstruct(new_the_ring, 'PolynomB',kn, K*(1 + dK(1)), 1, 2);
+        K = getcellstruct(new_the_ring, 'PolynomB', kp,1,2);
+        new_the_ring = setcellstruct(new_the_ring, 'PolynomB',kp, K*(1 + dK(2)), 1, 2);
+
+        
+        [~, tunes] = twissring(new_the_ring,0,1:length(new_the_ring)+1);
+        new_res_vec = (tunes-goal_tunes);
+        new_res = calc_rms(new_res_vec);
+        
+        if (new_res < res)
+            [mm, abort] = calc_tune_matrix(new_the_ring, kn, kp);
+            if abort, break; end
+            res_vec = new_res_vec;
+            res = new_res;
+            the_ring = new_the_ring;
+            tunesf = tunes;
+            factor = min([factor * 2, 1]);
+        else
+            factor = factor / 2;
+            %         [pseudoinv_mm, abort] = calc_tune_matrix(the_ring, knobs);
+            %         if abort, break; end
+        end
+        ii = ii + 1;
+    end
+    
+    converged = res < tolerancia;
+end
 end
 
-converged = res < tolerancia;
-
-
-function [pseudoinv_mm, abort] = calc_tune_matrix(the_ring,knobs)
+function [pseudoinv_mm, abort] = calc_tune_matrix_svd(the_ring,knobs)
 
 % calcula matriz de variacao do residuo
 dK = 1e-4;
@@ -102,3 +178,30 @@ if abort, return; end
 % calcula pseudo inversa da matriz de variacao do res�duo
 [U,S,V] = svd(mm,'econ');
 pseudoinv_mm = V*(S\U');
+end
+
+function [mm, abort] = calc_tune_matrix(the_ring, kn, kp)
+
+% calcula matriz de variacao do residuo
+dK = 1e-4;
+mm = zeros(2,2);
+
+calc_ring = the_ring;
+K = getcellstruct(calc_ring, 'PolynomB', kn,1,2);
+calc_ring = setcellstruct(calc_ring, 'PolynomB', kn, K*(1 + dK/2), 1, 2);
+[~, tunes_up] = twissring(calc_ring,0,1:length(calc_ring)+1);
+calc_ring = setcellstruct(calc_ring, 'PolynomB', kn, K*(1 - dK/2), 1, 2);
+[~, tunes_down] = twissring(calc_ring,0,1:length(calc_ring)+1);
+mm(:,1) = (tunes_up - tunes_down)/dK;
+
+calc_ring = the_ring;
+K = getcellstruct(calc_ring, 'PolynomB', kp,1,2);
+calc_ring = setcellstruct(calc_ring, 'PolynomB', kp, K*(1 + dK/2), 1, 2);
+[~, tunes_up] = twissring(calc_ring,0,1:length(calc_ring)+1);
+calc_ring = setcellstruct(calc_ring, 'PolynomB', kp, K*(1 - dK/2), 1, 2);
+[~, tunes_down] = twissring(calc_ring,0,1:length(calc_ring)+1);
+mm(:,2) = (tunes_up - tunes_down)/dK;
+
+
+abort = any(isnan(mm(:)));
+end
--- Release/lnls/fac_scripts/sirius/insertion_devices/optics_matching/run_insert_and_symm_ids.m
+++ Release/lnls/fac_scripts/sirius/insertion_devices/optics_matching/run_insert_and_symm_ids.m
@@ -1,4 +1,4 @@
-function run_insert_and_symm_ids
+function run_insert_and_symm_ids()
 
 % Loads Phase1 ID definitions
 ids_names = {};
@@ -10,73 +10,6 @@ ids_names{end+1} = 'caterete';  % SS-10 PHASE1
 ids_names{end+1} = 'ipe_hp';    % SS-11 PHASE1
 ids_names{end+1} = 'sabia_hp';  % SS-12 PHASE1
 ids_names{end+1} = 'manaca';    % SS-14 PHASE1
-             
-ids_phase1 = ids_select_80mm(ids_names);
-
-% Create nominal model
-sirius('SI.V20.01');
-the_ring0 = sirius_si_lattice();
-save('the_ring0.mat', 'the_ring0');
-
-% Calcs original twiss parameters
-twiss0 = calctwiss(the_ring0, 'n+1'); 
-tunes0 = [twiss0.mux(end), twiss0.muy(end)]/2/pi;
-mc = findcells(the_ring0,'FamName','mc');
-
-% Inserts IDs of Phase1
-strength = 1;
-the_ring1 = ids_insert_set(the_ring0, ids_phase1, strength);
-save('the_ring_nosym.mat', 'the_ring1');
-
-% % Checks effect of IDs on optics
-% r0 = calctwiss(the_ring0);
-% r1 = calctwiss(the_ring1);
-% fprintf('the_ring0: %.6f %.6f\n', r0.mux(end)/2/pi, r0.muy(end)/2/pi);
-% fprintf('the_ring1: %.6f %.6f\n', r1.mux(end)/2/pi, r1.muy(end)/2/pi);
-% figure; hold all; plot(r0.pos, r0.betax); plot(r1.pos, r1.betax); 
-% figure; hold all; plot(r0.pos, r0.betay); plot(r1.pos, r1.betay); 
-
-% Local symmetrization of each ID
-symm_loc.knobs       = {'qfa','qda','qfb','qdb1', 'qfp', 'qdp1'};
-symm_loc.nr_iters    = 20;
-symm_loc.tol         = 1e-6;
-symm_loc.goal        = [twiss0.betax(mc(1)),twiss0.alphax(mc(1)),...
-                       twiss0.betay(mc(1)),twiss0.alphay(mc(1)),...
-                       twiss0.etax(mc(1)), twiss0.etaxl(mc(1))];
-if isempty(ids_phase1)
-    symm_loc.id_sections = [];
-else
-    symm_loc.id_sections = getcellstruct(ids_phase1,'straight_number',1:length(ids_phase1));
-end
-the_ring2 = ids_locally_symmetrize(the_ring1, symm_loc);
-save('the_ring_locsym.mat', 'the_ring2');
-
-% Global symmetrization with tune fitting
-symm_glob.knobs      = {'qfa','qda','qfb','qdb1', 'qfp', 'qdp1'};
-symm_glob.id_sections = symm_loc.id_sections;
-symm_glob.nr_iters = 80;
-symm_glob.tol      = 1e-2;
-symm_glob.svs_lst  = [3,5,10,18];
-the_ring2          = ids_globally_symmetrize(the_ring2, tunes0, symm_glob);
-
-% Chromaticity correction
-the_ring2 = lnls_correct_chrom(the_ring2, [2.5,2.5]);
-
-% saves lattice model with ids
-save('the_ring_withids.mat', 'the_ring2');
-
-% % Checks effect of IDs on optics
-% r0 = calctwiss(the_ring0);
-% r2 = calctwiss(the_ring2);
-% fprintf('the_ring0: %.6f %.6f\n', r0.mux(end)/2/pi, r0.muy(end)/2/pi);
-% fprintf('the_ring2: %.6f %.6f\n', r2.mux(end)/2/pi, r2.muy(end)/2/pi);
-% figure; hold all; plot(r0.pos, r0.betax); plot(r2.pos, r2.betax); 
-% figure; hold all; plot(r0.pos, r0.betay); plot(r2.pos, r2.betay); 
-
-
-%% --- phase2 ---
-
-ids_names = {};
 ids_names{end+1} = 'caterete2'; % SS-02 
 ids_names{end+1} = 'ema2';      % SS-04        (with 3rd H cavity?)
 ids_names{end+1} = 'inga2';     % SS-05          
@@ -88,34 +21,55 @@ ids_names{end+1} = 'ipe2_hp';   % SS-17
 ids_names{end+1} = 'sabia2_hp'; % SS-18
 ids_names{end+1} = 'sabia4_hp'; % SS-19
 ids_names{end+1} = 'manaca2';   % SS-20
-
+             
 ids_phase2 = ids_select_80mm(ids_names);
 
-% Inserts IDs of Phase2
+
+%% Create nominal model
+sirius('SI.V20.01');
+the_ring0 = sirius_si_lattice();
+save('the_ring0.mat', 'the_ring0');
+
+
+
+%% Inserts IDs of Phase1
 strength = 1;
-the_ring2 = ids_insert_set(the_ring2, ids_phase2, strength);
+ring_nosym = ids_insert_set(the_ring0, ids_phase2, strength);
+save('the_ring_nosym.mat', 'ring_nosym');
 
-% Local symmetrization of each ID
-symm_loc.id_sections = [getcellstruct(ids_phase1,'straight_number',1:length(ids_phase1)); getcellstruct(ids_phase2, 'straight_number', 1:length(ids_phase2))];
-the_ring2 = ids_locally_symmetrize(the_ring2, symm_loc);
 
-% Local symmetrization of each ID
-symm_glob.id_sections = symm_loc.id_sections;
-symm_glob.nr_iters = 30;
-symm_glob.tol      = 1e-2;
-symm_glob.svs_lst  = [1,3,5,10,20,30,38];
-the_ring2           = ids_globally_symmetrize(the_ring2, tunes0, symm_glob);
 
-% Chromaticity correction
-the_ring2 = lnls_correct_chrom(the_ring2, [2.5,2.5]);
+%% Local symmetrization of each ID
+symm.knobs       = {'qfa','qda','qfb','qdb1','qdb2', 'qfp', 'qdp1', 'qdp2'};
+symm.nr_iters    = 20;
+symm.tol         = 1e-6;
+symm.look_tune   = false;
+symm.the_ring0   = the_ring0;
 
-% Checks effect of IDs on optics
-r0 = calctwiss(the_ring0);
-r2 = calctwiss(the_ring2);
-fprintf('the_ring0: %.6f %.6f\n', r0.mux(end)/2/pi, r0.muy(end)/2/pi);
-fprintf('the_ring2: %.6f %.6f\n', r2.mux(end)/2/pi, r2.muy(end)/2/pi);
-figure; hold all; plot(r0.pos, r0.betax); plot(r2.pos, r2.betax); 
-figure; hold all; plot(r0.pos, r0.betay); plot(r2.pos, r2.betay); 
+if isempty(ids),  symm.id_sections = [];
+else              symm.id_sections = getcellstruct(ids,'straight_number',1:length(ids));
+end
 
-% saves lattice model with ids
-save('the_ring_withids.mat', 'the_ring2');
+ring_sym = ids_symmetrize(ring_nosym, symm);
+save('the_ring_sym.mat', 'ring_sym');
+
+
+
+%% Tune correction
+fprintf('Correcting Tune...')
+[~, goal_tunes] = twissring(the_ring0,0,1:length(the_ring0)+1);
+[ring_symtune, conv] = lnls_correct_tunes(ring_sym,goal_tunes,[],'svd');
+if conv, fprintf(' converged.\n'); else fprintf(' did not converge.\n'); end
+
+
+%% Chromaticity correction
+disp('Correcting chromaticity...')
+ring_symtune = lnls_correct_chrom(ring_symtune, [2.5,2.5]);
+
+
+
+%% saves lattice model with ids
+save('the_ring_withids.mat', 'ring_symtune');
+disp('Symmetrized ring saved in file: the_ring_withids.mat')
+end
+
