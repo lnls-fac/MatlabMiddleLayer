743be618c7b476df9a4cd9510f80a10c37496750
ximenes <xresende@gmail.com>
2017-01-16 17:04:59 -0200

    Roll back default to SI.V21.01 (from SI.V21.02)

--- Release/lnls/startup_scripts/sirius.m
+++ Release/lnls/startup_scripts/sirius.m
@@ -13,7 +13,7 @@ Disconnect = false;
 default_tb_version = 'TB.V01.03' ;
 default_bo_version = 'BO.V03.02';
 default_ts_version = 'TS.V03.03' ;
-default_si_version = 'SI.V21.02' ;
+default_si_version = 'SI.V21.01' ;
 default_link       = 'NONE';
 
 default_version = default_si_version;
