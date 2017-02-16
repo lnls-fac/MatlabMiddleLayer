d58e0154d0a933808910d5fdc6eaf66bf20b0839
ximenes <xresende@gmail.com>
2017-02-16 16:52:50 -0200

    Add LOCO analysis for 2017-02-06 experiment

--- Release/machine/LNLS1/StorageRing/modes/lnls1_simulation_mode_IDsOFF_1p37GeV.m
+++ Release/machine/LNLS1/StorageRing/modes/lnls1_simulation_mode_IDsOFF_1p37GeV.m
@@ -1,9 +1,8 @@
 function lnls1_simulation_mode_IDsOFF_1p37GeV
 
-lnls1_set_id_field('AWG01', 0);
-lnls1_set_id_field('AWG09', 0);
-lnls1_set_id_field('AON11', 0);
-lnls1_set_id_field('AWS07', 0);
+lnls1_set_id_field('AWG01', 0.0);
+lnls1_set_id_field('AWG09', 0.2);
+lnls1_set_id_field('AON11', 0.0);
 setpv('QD',  'Physics', -2.966987743463266);
 setpv('QF',  'Physics',  2.769951576856493);
 setpv('QFC', 'Physics',  1.961093182324501);
