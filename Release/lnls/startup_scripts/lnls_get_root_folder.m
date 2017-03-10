49f3b109af5428fa080445a747aa715e752297c6
ximenes <xresende@gmail.com>
2017-03-10 15:51:25 -0300

    Change folder initializations
    - to be consistent with new location for the MML repository

--- Release/lnls/startup_scripts/lnls_get_root_folder.m
+++ Release/lnls/startup_scripts/lnls_get_root_folder.m
@@ -7,9 +7,11 @@ if strcmpi(computer, 'PCWIN') || strcmpi(computer, 'PCWIN64')
         root_folder = ['D:' filesep 'Arq' filesep 'fac_files'];
     end
 else
-    if exist('/home/fac_files','dir')
-        root_folder = '/home/fac_files';
+    if exist('/home/fac_files/lnls-fac','dir')
+        root_folder = '/home/fac_files/lnls-fac';
+    elseif exist('/home/fac_files/code','dir')
+        root_folder = '/home/fac_files/code';
     else
-        root_folder = '/Users/fac_files';
+        root_folder = '/Users/fac_files/code';
     end
 end
