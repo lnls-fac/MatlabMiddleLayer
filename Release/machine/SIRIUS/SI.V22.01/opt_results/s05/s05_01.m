1afc4ab905f48bf2ffb9b722b96545bbe2f08007
liulin <liulin>
2017-06-08 17:37:41 -0300

    Mode symmetrization after new B1 and B2 models

--- Release/machine/SIRIUS/SI.V22.01/opt_results/s05/s05_01.m
+++ Release/machine/SIRIUS/SI.V22.01/opt_results/s05/s05_01.m
@@ -1,33 +1,26 @@
-%% Version SI.V21-S05.03 
+%% Version SI.V22.01-S05.01 
 %  =====================
-%% 2017-03-28 - Liu
-% Optics assymetries introduced by new B1 and B2 models in previous version (SI.V21-S05.01) were corrected using MAD. In addition, 
-% the dispersion function has been corrected to zero at ID straights and first order optics in sectors B and P are now identical.
- 
 
-%% Version SI.V21-S05.01 
+%% Version SI.V22.01-S05.01 
 %  =====================
-% same optics as optimization run2_000491.m of tux49.tuy14.sext14.defConf
-%% 2017-02-02 New B1 and B2 models - Ximenes
-% B1 model-08: fieldmap '2017-02-01_B1_Model08_Sim_X=-32_32mm_Z=-1000_1000mm_Imc=451.8A.txt', init_rx is set to  4.860 mm at s=0   
-% B2 model-07: fieldmap '2017-02-01_B2_Model07_Sim_X=-63_27mm_Z=-1000_1000mm_Imc=451.8A.txt', init_rx is set to  5.444 mm at s=0
-% local script 'sirius_si_correct_tune_chrom' used.
-
+% The BPM upstream of dipole B2 was moved by 50mm (closer to B2) to fit in the girder. Modification in sectors C1 and C3. 
+% This change was requested by the Mechanical Design Group. 
+% Optics asymmetries introduced by new B1 and B2 models in previous version were corrected using MAD.
 
 % QUADRUPOLES
 % ===========
-QFA_strength  = +3.557799733269;
-QFB_strength  = +4.057287204045;
-QFP_strength  = +4.057287204045;
-QDA_strength  = -1.619644245089;
-QDB1_strength = -2.062972760584;
-QDB2_strength = -3.124763230447;
-QDP1_strength = -2.062972760584;
-QDP2_strength = -3.124763230447;
-Q1_strength   = +2.814083386204;
-Q2_strength   = +4.342691283291;
-Q3_strength   = +3.236518712541;
-Q4_strength   = +3.930660288075;
+QFA_strength  = +3.573152846456;
+QFB_strength  = +4.114808227974;
+QFP_strength  = +4.114808227974;
+QDA_strength  = -1.621537177175;
+QDB1_strength = -2.00636151367;
+QDB2_strength = -3.416409380519;
+QDP1_strength = -2.00636151367;
+QDP2_strength = -3.416409380519;
+Q1_strength   = +2.81580602167;
+Q2_strength   = +4.342314688125;
+Q3_strength   = +3.235627121213;
+Q4_strength   = +3.933913571031;
 
 % SEXTUPOLES
 % ==========
@@ -37,18 +30,18 @@ SDP0_strength = -64.942200000000000;
 SFA0_strength = +52.569600000000001;
 SFB0_strength = +73.740099999999998;
 SFP0_strength = +73.740099999999998;
-SDA1_strength = -164.290379560089548;
-SDB1_strength = -144.560891591434284;
-SDP1_strength = -143.529201102231468;
-SDA2_strength = -89.445119869211069;
-SDB2_strength = -123.497057498919773;
-SDP2_strength = -122.928948154723642;
-SDA3_strength = -140.526016852275035;
-SDB3_strength = -175.157535493184838;
-SDP3_strength = -174.791144298971204;
-SFA1_strength = +193.914825089860784;
-SFB1_strength = +232.069959601419356;
-SFP1_strength = +231.285894569649344;
-SFA2_strength = +152.134269401114096;
-SFB2_strength = +200.507468026663645;
-SFP2_strength = +199.875886055435359;
+SDA1_strength  = -162.9865;
+SDA2_strength  =  -88.8540;
+SDA3_strength  = -139.9408;
+SFA1_strength  =  191.8284;
+SFA2_strength  =  150.7692;
+SDB1_strength  = -141.6359;
+SDB2_strength  = -122.2281;
+SDB3_strength  = -173.8124;
+SFB1_strength  =  227.8813;
+SFB2_strength  =  197.8206;
+SDP1_strength  = -142.3060;
+SDP2_strength  = -122.2810;
+SDP3_strength  = -174.1574;
+SFP1_strength  =  229.2493;
+SFP2_strength  =  198.5034;
