cf11990555a7906438070bc19766d0cf2b2072a2
ximenes <xresende@gmail.com>
2017-10-10 15:51:45 -0300

    Improve sirius_integrated_strengths
    - also renamed it to sirius_nominal_optics

--- Release/machine/SIRIUS/common/sirius_nominal_optics.m
+++ Release/machine/SIRIUS/common/sirius_nominal_optics.m
@@ -1,7 +1,18 @@
-function sirius_si_integrated_strengths
+function sirius_nominal_optics
 
 global THERING;
+ats = atsummary;
 
+fprintf('--- tunes ---\n')
+fprintf('tunex: %011.8f\n', ats.tunes(1));
+fprintf('tuney: %011.8f\n', ats.tunes(2));
+
+fprintf('--- chrom ---\n')
+fprintf('chromx: %011.8f\n', ats.chromaticity(1));
+fprintf('chromy: %011.8f\n', ats.chromaticity(2));
+
+
+fprintf('--- quad intKL ---\n')
 fams = findmemberof('quad');
 for i=1:length(fams)
     q = fams{i};
@@ -12,7 +23,7 @@ for i=1:length(fams)
     fprintf('%8s: %+020.16f\n', q, kl);
 end
     
-
+fprintf('--- sext intSL ---\n')
 fams = findmemberof('sext');
 for i=1:length(fams)
     q = fams{i};
