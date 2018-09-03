69c36abf2da27db0449f21d50a49069e8e32c6c1
ximenes <xresende@gmail.com>
2018-09-03 12:04:08 -0300

    Update quad and sext init forces for meas models

--- Release/machine/SIRIUS/BO.V05.03/sirius_bo_models_from_measurements.m
+++ Release/machine/SIRIUS/BO.V05.03/sirius_bo_models_from_measurements.m
@@ -16,10 +16,10 @@ idx_qf = findcells(the_ring, 'FamName', 'QF');
 idx_qd = findcells(the_ring, 'FamName', 'QD');
 idx_sf = findcells(the_ring, 'FamName', 'SF');
 idx_sd = findcells(the_ring, 'FamName', 'SD');
-the_ring = setcellstruct(the_ring, 'PolynomB', idx_qf, +1.653749039924653, 1, 2);
-the_ring = setcellstruct(the_ring, 'PolynomB', idx_qd, -0.102503665792927, 1, 2);
-the_ring = setcellstruct(the_ring, 'PolynomB', idx_sf, +11.253921969396233, 1, 3);
-the_ring = setcellstruct(the_ring, 'PolynomB', idx_sd, +11.095043319723608, 1, 3);
+the_ring = setcellstruct(the_ring, 'PolynomB', idx_qf, +1.65374903807441, 1, 2);
+the_ring = setcellstruct(the_ring, 'PolynomB', idx_qd, -0.10250366405148, 1, 2);
+the_ring = setcellstruct(the_ring, 'PolynomB', idx_sf, +11.25394814115368, 1, 3);
+the_ring = setcellstruct(the_ring, 'PolynomB', idx_sd, +11.09496614284700, 1, 3);
 ats = atsummary(the_ring);
 if any(abs(ats.tunes - goal_tunes) > 0.00001) || any(abs(ats.chromaticity - goal_chrom) > 0.01)
     for i=1:8
@@ -27,10 +27,10 @@ if any(abs(ats.tunes - goal_tunes) > 0.00001) || any(abs(ats.chromaticity - goal
         [the_ring, conv, t2, t1] = lnls_correct_tunes(the_ring, goal_tunes, {'QF','QD'}, 'svd', 'add', 10, 1e-9);
     end
     fprintf('   Tunes and Chromaticities corrected!\n');
-    fprintf('   qf_strength = %f;\n', the_ring{idx_qf(1)}.PolynomB(2));
-    fprintf('   qd_strength = %f;\n', the_ring{idx_qd(1)}.PolynomB(2));
-    fprintf('   sf_strength = %f;\n', the_ring{idx_sf(1)}.PolynomB(3));
-    fprintf('   sd_strength = %f;\n', the_ring{idx_sd(1)}.PolynomB(3));
+    fprintf('   qf_strength = %+.14f;\n', the_ring{idx_qf(1)}.PolynomB(2));
+    fprintf('   qd_strength = %+.14f;\n', the_ring{idx_qd(1)}.PolynomB(2));
+    fprintf('   sf_strength = %+.14f;\n', the_ring{idx_sf(1)}.PolynomB(3));
+    fprintf('   sd_strength = %+.14f;\n', the_ring{idx_sd(1)}.PolynomB(3));
 end
 
 
