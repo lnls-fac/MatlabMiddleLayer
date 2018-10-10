85358e712d59f54bff717963688c5d922bc0f22b
Murilo Alves <alvesb.murilo@gmail.com>
2018-10-10 16:48:37 -0300

    First turn correction improvement

--- Release/applications/lnls/+sirius_commis/+first_turns/+bo/check_first_turn.m
+++ Release/applications/lnls/+sirius_commis/+first_turns/+bo/check_first_turn.m
@@ -1,4 +1,6 @@
-function check_first_turn(machine, n_mach, param, n_part, n_pulse, n_turns)
+function [machine_correct, first_turn_mach, first_turn_mach_refined, count_turns] = check_first_turn(machine, n_mach, param, param_errors, n_part, n_pulse, n_turns, n0, M_acc)
+
+sirius_commis.common.initializations();
 
 if n_mach == 1
     machine_cell = {machine};
@@ -8,6 +10,11 @@ elseif n_mach > 1
     param_cell = param;
 end
 
+% machine_correct = cell(n_mach, 1);
+first_turn_mach = zeros(n_mach, 1);
+first_turn_mach_refined = zeros(n_mach, 1);
+count_turns = ones(n_pulse, n_mach) * n_turns;
+
 for j = 1:n_mach    
     fprintf('=================================================\n');
     fprintf('MACHINE NUMBER %i \n', j)
@@ -16,6 +23,23 @@ for j = 1:n_mach
     machine = machine_cell{j};
     param = param_cell{j};
     
+    n_min = min(n0(:, j));
+    
+    if n_min < n_turns
+        machine_cell{j} = sirius_commis.first_turns.bo.first_turn_corrector(machine, 1, param, param_errors, M_acc, n_part, n_pulse);
+        machine_cell{j} = machine_cell{j}{1};
+        first_turn_mach(j) = 1;
+        count_turns(:, j) = sirius_commis.first_turns.bo.multiple_pulse_turn(machine_cell{j}, 1, param, param_errors, n_part, n_pulse, n_turns);
+        
+        if min(count_turns(:, j)) < n_turns
+            machine_cell{j} = sirius_commis.first_turns.bo.first_turn_corrector(machine, 1, param, param_errors, M_acc, n_part, 10*n_pulse);
+            machine_cell{j} = machine_cell{j}{1};
+            first_turn_mach_refined(j) = 1;
+            count_turns(:, j) = sirius_commis.first_turns.bo.multiple_pulse_turn(machine_cell{j}, 1, param, param_errors, n_part, n_pulse, n_turns);
+        end
+    end
 end
+
+machine_correct = machine_cell;
 end
 
