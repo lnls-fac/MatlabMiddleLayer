88c38eb4a3134bbd497c954097d6cadb4e5428c2
Murilo Alves <alvesb.murilo@gmail.com>
2018-12-14 10:26:22 -0200

    Generalization for different number of CH and CV

--- Release/applications/lnls/+sirius_commis/+first_turns/+si/check_first_turn.m
+++ Release/applications/lnls/+sirius_commis/+first_turns/+si/check_first_turn.m
@@ -1,9 +1,9 @@
-function [machine_correct, first_turn_mach, first_turn_mach_refined, count_turns, gr_mach] = check_first_turn(machine, n_mach, param, param_errors, n_part, n_pulse, n_turns, n0, n_sv, M_acc)
+function [machine_correct, first_turn_mach, first_turn_mach_refined, count_turns, gr_mach, n_t] = check_first_turn(machine, n_mach, param, param_errors, n_part, n_pulse, n_turns, n_pulse_turns, n0, n_sv, M_acc)
 % Increases the intensity of BPMs and adjusts the first turn by changing the
 % correctors based on BPMs measurements
 %
 % INPUTS:
-%  - machine: booster ring model with errors
+%  - machine: storage ring model with errors
 %  - n_mach: number of machines
 %  - param: cell of structs with adjusted injection parameters for each
 % machine
@@ -12,11 +12,11 @@ function [machine_correct, first_turn_mach, first_turn_mach_refined, count_turns
 %  - n_pulse: number of pulses to average
 %  - n_turns: number of turns to check after first turn correction
 %  - n0: number of turns for each injection pulse without first turn correction
-%  - MS_acc: transfer matrix from the origin (InjSept) to all the elements
+%  - M_acc: transfer matrix from the origin (InjSept) to all the elements
 %  of machine ([~, MS] = findm44())
 %
 % OUTPUTS:
-%  - machine_correct: booster ring model with corrector setup adjusted for 1st turn
+%  - machine_correct: storage ring model with corrector setup adjusted for 1st turn
 %  - first_turns_mach: 1 if it was possible to correct first turn with
 %  given number of pulses 
 %  - first_turns_mach_refined: 1 if it was needed to increase the pulses
@@ -24,7 +24,7 @@ function [machine_correct, first_turn_mach, first_turn_mach_refined, count_turns
 %  - gr_mach: 1 indicates that the corresponding machine demanded a kick in
 %  correctors greater than the limit
 %
-% Version 1 - Murilo B. Alves - October, 2018
+% Version 1 - Murilo B. Alves - December, 2018
 
 sirius_commis.common.initializations();
 
@@ -39,10 +39,56 @@ end
 % machine_correct = cell(n_mach, 1);
 first_turn_mach = zeros(n_mach, 1);
 first_turn_mach_refined = zeros(n_mach, 1);
-count_turns = ones(3, n_mach) * n_turns;
+count_turns = zeros(n_pulse_turns, n_mach);
 gr_mach = cell(n_mach, 2);
 machine_correct = cell(n_mach, 1);
 param_errors.sigma_bpm = 2e-3;
+
+fam = sirius_si_family_data(machine_cell{1});
+ch = fam.CH.ATIndex;
+cv = fam.CV.ATIndex;
+bpm = fam.BPM.ATIndex;
+m_corr_x = zeros(length(bpm), length(ch));
+m_corr_y = zeros(length(bpm), length(cv));
+
+
+M_bpms_x = M_acc(1:2, 1:2, bpm);
+M_bpms_y = M_acc(3:4, 3:4, bpm);
+
+for j = 1:length(ch)
+    ind_bpms_ch = bpm > ch(j);
+    first = find(ind_bpms_ch);
+
+    if ~isempty(first)
+        first = first(1);
+        trecho = first:length(bpm);
+        M_ch = M_acc(1:2, 1:2, ch(j));
+        for i=1:length(trecho)
+            M_x = M_bpms_x(:, :, trecho(i)) / M_ch; 
+            m_corr_x(trecho(i), j) = squeeze(M_x(1, 2, :));
+        end
+    end
+end
+
+for j = 1:length(cv)
+    ind_bpms_cv = bpm > cv(j);
+    first = find(ind_bpms_cv);
+
+    if ~isempty(first)
+        first = first(1);
+        trecho = first:length(bpm);
+        M_cv = M_acc(3:4, 3:4, cv(j));
+        for i=1:length(trecho)
+            M_y = M_bpms_y(:, :, trecho(i)) / M_cv; 
+            m_corr_y(trecho(i), j) = squeeze(M_y(1, 2, :));
+        end
+    end
+end
+
+m0xy = zeros(size(bpm, 1), size(cv, 1));
+m0yx = zeros(size(bpm, 1), size(ch, 1));
+n_t = zeros(n_mach, 1);
+m_corr = [m_corr_x, m0xy; m0yx, m_corr_y];
 
 for j = 1:n_mach    
     fprintf('=================================================\n');
@@ -52,23 +98,39 @@ for j = 1:n_mach
     machine = machine_cell{j};
     param = param_cell{j};
     
+    machine = setcavity('off', machine);
+    machine = setradiation('on', machine);
+    
     n_min = min(n0(:, j));
     
     if n_min < n_turns
         % [machine_correct{j}, ~, gr_mach{j, 1}, gr_mach{j, 2}] = sirius_commis.first_turns.bo.first_turn_corrector(machine, 1, param, param_errors, M_acc, n_part, n_pulse);
-        machine_correct{j} = sirius_commis.first_turns.si.first_turn_corrector(machine, 1, param, param_errors, M_acc, n_part, 10, n_sv);
-        machine_correct{j} = machine_correct{j}{1};
+        [machine_correct{j}, ~, ~, ~, ~, n_t(j)] = sirius_commis.first_turns.si.first_turn_corrector(machine, 1, param, param_errors, m_corr, n_part, n_pulse, n_sv);
+        machine_correct1 = machine_correct{j}{1};
         first_turn_mach(j) = 1;
-        [count_turns(:, j), r_bpm, int_bpm] = sirius_commis.first_turns.si.multiple_pulse_turn(machine_correct{j}, 1, param, param_errors, n_part, 3, n_turns);
+        count_turns1 = sirius_commis.first_turns.si.multiple_pulse_turn(machine_correct1, 1, param, param_errors, n_part, n_pulse_turns, n_turns);
         
-        %if min(count_turns(:, j)) < n_turns
-            % [machine_correct{j}, ~, gr_mach{j, 1}, gr_mach{j, 2}] = sirius_commis.first_turns.bo.first_turn_corrector(machine, 1, param, param_errors, M_acc, n_part, 10*n_pulse, r_bpm, int_bpm);
-        %    machine_correct{j} = sirius_commis.first_turns.si.first_turn_corrector(machine, 1, param, param_errors, M_acc, n_part, 10*n_pulse);
-        %    machine_correct{j} = machine_correct{j}{1};
-        %    first_turn_mach_refined(j) = 1;
-        %    count_turns(:, j) = sirius_commis.first_turns.si.multiple_pulse_turn(machine_correct{j}, 1, param, param_errors, n_part, n_pulse, n_turns);
-        %end
+        if min(count_turns1) < n_turns
+            [machine_correct{j}, ~, ~, ~, ~, n_t(j)] = sirius_commis.first_turns.si.first_turn_corrector(machine_correct1, 1, param, param_errors, m_corr, n_part, 5*n_pulse, n_t(j));
+            % machine_correct{j} = sirius_commis.first_turns.si.first_turn_corrector(machine, 1, param, param_errors, M_acc, n_part, 10*n_pulse);
+            machine_correct2 = machine_correct{j}{1};
+            first_turn_mach_refined(j) = 1;
+            count_turns2 = sirius_commis.first_turns.si.multiple_pulse_turn(machine_correct2, 1, param, param_errors, n_part, n_pulse_turns, n_turns);
+            if min(count_turns1) > min(count_turns2)
+                machine_correct{j} = machine_correct1;
+                count_turns(:, j) = count_turns1;
+            else
+                machine_correct{j} = machine_correct2;
+                count_turns(:, j) = count_turns2;
+            end
+        else
+            machine_correct{j} = machine_correct1;
+            count_turns(:, j) = count_turns1;
+        end
+    else
+        machine_correct{j} = machine;        
     end
-end
+    
+    save first_turns.mat machine_correct first_turn_mach first_turn_mach_refined count_turns gr_mach n_t
 end
 
--- Release/applications/lnls/+sirius_commis/+first_turns/+si/correct_orbit_bpm_matrix.m
+++ Release/applications/lnls/+sirius_commis/+first_turns/+si/correct_orbit_bpm_matrix.m
@@ -1,4 +1,4 @@
-function [machine, r_bpm, gr_mach_x, gr_mach_y] = correct_orbit_bpm_matrix(machine, param, param_errors, MS_acc, n_part, n_pulse, n_sv, r_bpm, int_bpm)
+function [machine, r_bpm, gr_mach_x, gr_mach_y, n_t] = correct_orbit_bpm_matrix(machine, param, param_errors, m_corr, n_part, n_pulse, n_sv, r_bpm, int_bpm)
 % Increases the intensity of BPMs and adjusts the orbit by changing the
 % correctors based on BPMs measurements with a matrix approach
 %
@@ -6,8 +6,8 @@ function [machine, r_bpm, gr_mach_x, gr_mach_y] = correct_orbit_bpm_matrix(machi
 %  - machine: booster ring model with errors
 %  - param: cell of structs with adjusted injection parameters for each
 % machine
-%  - MS_acc: transfer matrix from the origin (InjSept) to all the elements
-%  of machine ([~, MS] = findm44())
+%  - m_corr: first turn trajectory corrector matrix calculate from transfer
+%  matrix as a response matrix
 %  - n_part: number of particles
 %  - n_pulse: number of pulses to average
 %
@@ -26,75 +26,33 @@ fam = sirius_si_family_data(machine);
 ch = fam.CH.ATIndex;
 cv = fam.CV.ATIndex;
 bpm = fam.BPM.ATIndex;
-m_corr_x = zeros(length(bpm), length(ch));
-m_corr_y = zeros(length(bpm), length(cv));
+m_corr_x = m_corr(1:size(bpm, 1), 1:size(ch, 1));
+m_corr_y = m_corr(size(bpm, 1)+1:end, size(ch, 1)+1:end);
 
-theta_x = zeros(length(ch), 1);
-theta_y = zeros(length(cv), 1);
+theta_x = lnls_get_kickangle(machine, ch, 'x')';
+theta_y = lnls_get_kickangle(machine, cv, 'y')';
 gr_mach_x = zeros(length(ch), 1);
 gr_mach_y = zeros(length(cv), 1);
-
+param.orbit = findorbit4(machine, 0, 1:length(machine));
 if ~exist('r_bpm', 'var') && ~exist('int_bpm', 'var')
     [~, ~, ~, r_bpm, int_bpm] = sirius_commis.injection.si.multiple_pulse(machine, param, param_errors, n_part, n_pulse, length(machine), 'on', 'diag');
 end
 
-M_bpms_x = MS_acc(1:2, 1:2, bpm);
-M_bpms_y = MS_acc(3:4, 3:4, bpm);
-
 rms_orbit_x_bpm_old = nanstd(r_bpm(1,:));
 rms_orbit_y_bpm_old = nanstd(r_bpm(2,:));
-
- corr_lim = 310e-6;
-
-for j = 1:length(ch)
-    ind_bpms_ch = bpm > ch(j);
-    first = find(ind_bpms_ch);
-
-    if ~isempty(first)
-        first = first(1);
-        trecho = first:length(bpm);
-        M_ch = MS_acc(1:2, 1:2, ch(j));
-        for i=1:length(trecho)
-            M_x = M_bpms_x(:, :, trecho(i)) / M_ch; 
-            m_corr_x(trecho(i), j) = squeeze(M_x(1, 2, :));
-        end
-    end
-
-    ind_bpms_cv = bpm > cv(j);
-    first = find(ind_bpms_cv);
-
-    if ~isempty(first)
-        first = first(1);
-        trecho = first:length(bpm);
-        M_cv = MS_acc(3:4, 3:4, cv(j));
-        for i=1:length(trecho)
-            M_y = M_bpms_y(:, :, trecho(i)) / M_cv; 
-            m_corr_y(trecho(i), j) = squeeze(M_y(1, 2, :));
-        end
-    end
-
-end
+corr_lim = 310e-6;
 
 eff_lim = 1;
-% if n_pulse <= 10
-   % pos_lim = param_errors.sigma_bpm / eff_lim / sqrt(n_pulse);
-% else
- %   pos_lim = 1e-3;
-%end
 n_cor = 1;
 
 rms_orbit_x_bpm_new = rms_orbit_x_bpm_old;
 rms_orbit_y_bpm_new = rms_orbit_y_bpm_old;
 inc_x = true; inc_y = true;
-stop_x = false; stop_y = false;
 
-while int_bpm(end) < eff_lim  ||  inc_x || inc_y
+while int_bpm(end) < eff_lim
     bpm_int_ok = bpm(int_bpm > 0.80);
     [~, ind_ok_bpm] = intersect(bpm, bpm_int_ok);
-    
-    rms_orbit_x_bpm_old = rms_orbit_x_bpm_new;
-    rms_orbit_y_bpm_old = rms_orbit_y_bpm_new;
-
+   
     m_corr_x_ok = m_corr_x(ind_ok_bpm, :);
     [Ux, Sx, Vx] = svd(m_corr_x_ok, 'econ');
     Sx_inv = 1 ./ diag(Sx);
@@ -147,48 +105,128 @@ while int_bpm(end) < eff_lim  ||  inc_x || inc_y
     rms_orbit_x_bpm_new = nanstd(r_bpm(1,:));
     rms_orbit_y_bpm_new = nanstd(r_bpm(2,:));
     
-    if int_bpm(end) >= eff_lim
-        n_cor = 1;
-        if ~stop_x
-            inc_x = rms_orbit_x_bpm_new < rms_orbit_x_bpm_old;
-            if ~inc_x
-                theta_x = theta_x + m_corr_inv_x * x_bpm';
-                machine = lnls_set_kickangle(machine, theta_x, ch, 'x');
-                stop_x = true;
-            end
-        end
-        if ~stop_y
-            inc_y = rms_orbit_y_bpm_new < rms_orbit_y_bpm_old;
-            if ~inc_y  
-                theta_y = theta_y + m_corr_inv_y * y_bpm';
-                machine = lnls_set_kickangle(machine, theta_y, cv, 'y');
-                stop_y = true;
-            end
-        end
-    elseif int_bpm(end) < eff_lim && n_cor > 5
+    if n_cor > 5
        n_sv = n_sv - 5;
        machine = lnls_set_kickangle(machine, zeros(length(ch), 1), ch, 'x');
        machine = lnls_set_kickangle(machine, zeros(length(cv), 1), cv, 'y');
        n_cor = 1;
        warning('Number of Singular Values reduced')
-       if n_sv == 0
+       if n_sv < 1
            error('Problems in Singular Values')
        end
     end
     n_cor = n_cor + 1;
 end
+
+[U, S, V] = svd(m_corr, 'econ');
+S_inv = 1 ./ diag(S);
+S_inv(isinf(S_inv)) = 0;
+n_t = n_sv;
+k = 1;
+n_inc = 0;
+stop_x = false; stop_y = false;
+sv_change = false;
+
+theta_x_i = theta_x;
+theta_y_i = theta_y;
+
+while inc_x || inc_y
+    S_inv_var = S_inv;
+    
+    if ~sv_change
+        if ~stop_x
+            rms_orbit_x_bpm_old = rms_orbit_x_bpm_new;
+        end
+        if ~stop_y
+            rms_orbit_y_bpm_old = rms_orbit_y_bpm_new;
+        end
+    end
+    
+    S_inv_var(n_t + 1:end) = 0;
+    % Sx_inv(Sx_inv > 5 * Sx_inv(1)) = 0;
+    S_inv_var = diag(S_inv_var);
+    m_corr_inv = V * S_inv_var * U';
+    
+    m_corr_inv_x = m_corr_inv(1:size(ch, 1), 1:size(bpm, 1));
+    m_corr_inv_y = m_corr_inv(size(ch)+1:end, size(bpm, 1)+1:end);
+    
+    if inc_x 
+        x_bpm = squeeze(r_bpm(1, :));
+        theta_x_f =  theta_x_i - m_corr_inv_x * x_bpm';
+        over_kick_x = abs(theta_x_f) > corr_lim;
+    
+        if any(over_kick_x)
+            warning('Horizontal corrector kick greater than maximum')
+            gr_mach_x(over_kick_x) = 1;
+            theta_x_f(over_kick_x) =  sign(theta_x_f(over_kick_x)) * corr_lim;
+        end
+        
+        machine = lnls_set_kickangle(machine, theta_x_f, ch, 'x');
+        fprintf('HORIZONTAL CORRECTION \n');
+        
+        theta_x_i = theta_x_f;
+    end
+    
+    if inc_y
+        y_bpm = squeeze(r_bpm(2, :));
+        theta_y_f = theta_y_i - m_corr_inv_y * y_bpm';
+        over_kick_y = abs(theta_y_f) > corr_lim;
+    
+        if any(over_kick_y)
+            warning('Vertical corrector kick greater than maximum')
+            gr_mach_y(over_kick_y) = 1;
+            theta_y_f(over_kick_y) =  sign(theta_y_f(over_kick_y)) * corr_lim;
+        end
+        
+        machine = lnls_set_kickangle(machine, theta_y_f, cv, 'y');
+        fprintf('VERTICAL CORRECTION \n');
+        
+        theta_y_i = theta_y_f;
+    end
+    
+    param.orbit = findorbit4(machine, 0, 1:length(machine));
+
+    [~, ~, ~, r_bpm, ~] = sirius_commis.injection.si.multiple_pulse(machine, param, param_errors, n_part, n_pulse, length(machine), 'on', 'diag');
+    
+    rms_orbit_x_bpm_new = nanstd(r_bpm(1,:));
+    rms_orbit_y_bpm_new = nanstd(r_bpm(2,:));
+    
+    if ~stop_x
+        inc_x = rms_orbit_x_bpm_new < rms_orbit_x_bpm_old;
+        if ~inc_x
+            machine = lnls_set_kickangle(machine, theta_x_i, ch, 'x');
+            stop_x = true;
+        end
+    end
+    
+    if ~stop_y
+        inc_y = rms_orbit_y_bpm_new < rms_orbit_y_bpm_old;
+        if ~inc_y
+            machine = lnls_set_kickangle(machine, theta_y_i, cv, 'y');
+            stop_y = true;
+        end
+    end
+    
+    if ~inc_x && ~inc_y
+        if n_inc == 0
+            % theta_x = theta_x + m_corr_inv_x * x_bpm';
+            % theta_y = theta_y + m_corr_inv_y * y_bpm';
+            % machine = lnls_set_kickangle(machine, theta_x, ch, 'x');
+            % machine = lnls_set_kickangle(machine, theta_y, cv, 'y');
+            return
+        else
+            n_t = n_sv + k * 10;
+            inc_x = true; inc_y = true;
+            stop_x = false; stop_y = false;
+            k = k+1;
+            n_inc = 0;
+            sv_change = true;
+            fprintf('Number of Singular Values: %i \n', n_t);
+            continue
+        end
+    end
+    n_inc = n_inc + 1;
+    sv_change = false;
 end
-% [rms_orbit_bpm, max_orbit_bpm] = calc_rms(r_bpm);
-
-% function [rms_orbit_bpm, max_orbit_bpm] = calc_rms(r_bpm)
-%     rms_orbit_x_bpm = nanstd(r_bpm(1,:));
-%     rms_orbit_y_bpm = nanstd(r_bpm(2,:));
-%     [max_orbit_x_bpm, i_max_x] = nanmax(abs(r_bpm(1,:)));
-%     max_orbit_x_bpm = sign(r_bpm(1, i_max_x)) * max_orbit_x_bpm;
-%     [max_orbit_y_bpm, i_max_y] = nanmax(abs(r_bpm(2,:)));
-%     max_orbit_y_bpm = sign(r_bpm(2, i_max_y)) * max_orbit_y_bpm;
-%     rms_orbit_bpm = [rms_orbit_x_bpm, rms_orbit_y_bpm];
-%     max_orbit_bpm = [max_orbit_x_bpm, max_orbit_y_bpm];
-% end
-
-
+param.orbit = findorbit4(machine, 0, 1:length(machine));
+end
