af43e2879b20bcacdec7a48afd46ca569d2cf582
Murilo Alves <alvesb.murilo@gmail.com>
2019-01-22 15:05:41 -0200

    Function to turn off quadrupoles and sextupoles

--- Release/applications/lnls/+sirius_commis/+injection/+si/turn_off_magnets.m
+++ Release/applications/lnls/+sirius_commis/+injection/+si/turn_off_magnets.m
@@ -1,8 +1,30 @@
-function si_ring2 = turn_off_magnets(si_ring)
+function [si_ring2, param] = turn_off_magnets(si_ring, param)
 
 fam = sirius_si_family_data(si_ring);
 si_ring2 = si_ring;
 
+% paSN = getcellstruct(si_ring2, 'PolynomA', fam.SN.ATIndex);
+%paQN = getcellstruct(si_ring2, 'PolynomA', fam.QN.ATIndex);
+%pbSN = getcellstruct(si_ring2, 'PolynomB', fam.SN.ATIndex);
+%pbQN = getcellstruct(si_ring2, 'PolynomB', fam.QN.ATIndex);
+
+% paSN = si_ring{fam.SN.ATIndex}.PolynomA;
+% paSN = si_ring{fam.SN.ATIndex}.PolynomB;
+
+for i = 1:length(fam.SN.ATIndex)
+    paSN = si_ring{fam.SN.ATIndex(i)}.PolynomA;
+    pbSN = si_ring{fam.SN.ATIndex(i)}.PolynomB;
+    si_ring2 = setcellstruct(si_ring2, 'PolynomA', fam.SN.ATIndex(i), {0*paSN});
+    si_ring2 = setcellstruct(si_ring2, 'PolynomB', fam.SN.ATIndex(i), {0*pbSN});
+end
+for ii = 1:length(fam.QN.ATIndex)
+    paQN = si_ring{fam.QN.ATIndex(ii)}.PolynomA;
+    pbQN = si_ring{fam.QN.ATIndex(ii)}.PolynomB;
+    si_ring2 = setcellstruct(si_ring2, 'PolynomA', fam.QN.ATIndex(ii), {0*paQN});
+    si_ring2 = setcellstruct(si_ring2, 'PolynomB', fam.QN.ATIndex(ii), {0*pbQN});
+end
+
+%{
 SFA0 = fam.SFA0.ATIndex(1);
 QFA = fam.QFA.ATIndex(1);
 SDA0 = fam.SDA0.ATIndex(1);
@@ -37,5 +59,28 @@ si_ring2 = setcellstruct(si_ring2, 'PolynomA', SDA0, {zeros(1, length(paSDA0{1})
 si_ring2 = setcellstruct(si_ring2, 'PolynomA', QDA, {zeros(1, length(paQDA{1}))});
 si_ring2 = setcellstruct(si_ring2, 'PolynomA', SDA1, {zeros(1, length(paSDA1{1}))});
 si_ring2 = setcellstruct(si_ring2, 'PolynomA', Q1, {zeros(1, length(paQ1{1}))});
+%}
+
+si_twiss = calctwiss(si_ring2);
+%{
+param.twiss.betax0 = si_twiss.betax(1);
+param.twiss.betay0 = si_twiss.betay(1);
+param.twiss.alphax0 = si_twiss.alphax(1);
+param.twiss.alphay0 = si_twiss.alphay(1);
+param.twiss.etax0 = si_twiss.etax(1);
+param.twiss.etay0 = si_twiss.etay(1);
+param.twiss.etaxl0 = si_twiss.etaxl(1);
+param.twiss.etayl0 = si_twiss.etayl(1);
+%}
+
+bpms = fam.BPM.ATIndex;
+delta = 1e-5;
+r_init_n = [0; 0; 0; 0; -delta; 0];
+r_final_n = linepass(si_ring, r_init_n, bpms);
+r_init_p = [0; 0; 0; 0; +delta; 0];
+r_final_p = linepass(si_ring, r_init_p, bpms);
+x_n = r_final_n(1, :);
+x_p = r_final_p(1, :);
+param.etax_bpms = (x_p - x_n) ./ 2 ./ delta;
 end
 
