ba5179034818caed6917ea3f64cc96ac1cc36656
Murilo B. Alves <alvesb.murilo@gmail.com>
2019-02-07 14:42:33 -0200

    Change default directory to /home/fac

--- Release/lnls/startup_scripts/lnls_get_root_folder.m
+++ Release/lnls/startup_scripts/lnls_get_root_folder.m
@@ -2,13 +2,13 @@ function root_folder = lnls_get_root_folder()
 
 if strcmpi(computer, 'PCWIN') || strcmpi(computer, 'PCWIN64')
     if exist(['C:' filesep 'Arq'], 'file')
-        root_folder = ['C:' filesep 'Arq' filesep 'fac_files'];
+        root_folder = ['C:' filesep 'Arq' filesep 'fac'];
     else
-        root_folder = ['D:' filesep 'Arq' filesep 'fac_files'];
+        root_folder = ['D:' filesep 'Arq' filesep 'fac'];
     end
 else
-    if exist('/home/fac_files/lnls-fac','dir')
-        root_folder = '/home/fac_files/lnls-fac';
+    if exist('/home/fac','dir')
+        root_folder = '/home/fac';
     elseif exist('/home/fac_files/code','dir')
         root_folder = '/home/fac_files/code';
     else
