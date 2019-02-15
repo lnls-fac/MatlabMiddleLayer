586f60e2e6deda144163e56fc42642d281d3d7b4
Murilo B. Alves <alvesb.murilo@gmail.com>
2019-02-15 15:37:45 -0200

    Functions to analyze BBA results

--- Release/applications/lnls/+sirius_commis/+first_turns/+si/eff_bba.m
+++ Release/applications/lnls/+sirius_commis/+first_turns/+si/eff_bba.m
@@ -1,35 +1,61 @@
-function stat_bba = eff_bba(offset_bba, offset_quad, offset_bpm, n_mach)
-   
-stat_bba = cell(10, 1);
+function [stat_bba, mach_bba] = eff_bba(offset_bba, offset_quad, offset_bpm, n_mach)
 
-for j = 1:n_mach
-    
-    um = 1e6;
-    
+stat_bba = cell(n_mach, 1);
+um = 1e6;
+for j = 1:n_mach    
     offset_bbaj = offset_bba{j} * um;
+    % derro(offset_bbaj == 0 ) = 0;
+    % offset_bbaj = offset_bbaj + derro;
     offset_quadj = offset_quad{j} * um;
     offset_bpmj = offset_bpm{j} * um;
+    dcrs = zeros(length(offset_bpmj(:, 1)), length(offset_bpmj(1, :)));
+
+    offset_bpmj(offset_bbaj == 0) = NaN;
+    offset_quadj(offset_bbaj == 0) = NaN;
+    % rms = nanstd(offset_bbaj);
+    % offset_bbaj(abs(offset_bbaj) > rms(1)) = 0;
+    % offset_bbaj(abs(offset_bbaj) < rms(1)) = 0;
+    initial_bba = offset_quadj - offset_bpmj;
+    new_offset = offset_bpmj + offset_bbaj;
+    final_bba = offset_quadj - new_offset;
+    dcrs(final_bba <= initial_bba) = 1;
+    dcrs(offset_bbaj == 0) = NaN;
+    v_dcrs = abs(final_bba) - abs(initial_bba);
+    v_dcrs(offset_bbaj == 0) = NaN;
+    % dcrs(isnan(dcrs)) = 0;
+    % offset_bbaj(~dcrs) = NaN;
+    % initial_dif = abs(offset_quadj - (-offset_bpmj));
+    % new_offset = offset_bpmj - offset_bbaj;
+    % final_dif = abs(offset_quadj - (-new_offset));
     
-    offset_quad_bba = offset_bbaj - offset_bpmj;
-    offset_quad_bba(offset_bbaj == 0) = NaN;
-    stat_bba{j}.offset_quad_bba = offset_quad_bba;
-    stat_bba{j}.dif_quad_bba = offset_quad_bba - offset_quadj;
-    stat_bba{j}.ave_bba_quad = nanmean(stat_bba{j}.dif_quad_bba);
-    stat_bba{j}.rms_bba_quad = nanstd(stat_bba{j}.dif_quad_bba);
-    initial_dif = abs(offset_bpmj - offset_quadj);
-    offset_bpm_new = offset_bpmj - offset_bbaj;
-    final_dif = abs(offset_bpm_new - offset_quadj);
+    final_bba(offset_bbaj == 0) = NaN;
+    initial_bba(offset_bbaj == 0) = NaN;
     
-    final_dif(offset_bbaj == 0) = NaN;
-    initial_dif(offset_bbaj == 0) = NaN;
-    
-    stat_bba{j}.initial_dif = initial_dif;
-    stat_bba{j}.final_dif = final_dif;
+    stat_bba{j}.initial_dif = initial_bba;
+    stat_bba{j}.final_dif = final_bba;
     stat_bba{j}.ave_initial_dif = nanmean(stat_bba{j}.initial_dif);
     stat_bba{j}.rms_initial_dif = nanstd(stat_bba{j}.initial_dif);
     stat_bba{j}.ave_final_dif = nanmean(stat_bba{j}.final_dif);
     stat_bba{j}.rms_final_dif = nanstd(stat_bba{j}.final_dif);
+    stat_bba{j}.bba_ok = dcrs;
+    stat_bba{j}.decrease_value = v_dcrs;
 end
+
+for k = 1:size(offset_bbaj, 1)
+    for j = 1:n_mach
+        bba_mach_init(j, :) = stat_bba{j}.initial_dif(k, :);
+        bba_mach_final(j, :) = stat_bba{j}.final_dif(k, :);
+    end
+    init_dif_mach(k, :) = nanstd(bba_mach_init, 1);
+    final_dif_mach(k, :) = nanstd(bba_mach_final, 1);
+end
+
+mach_bba.init_dif_mach = init_dif_mach;
+mach_bba.final_dif_mach = final_dif_mach;
+% decrease = init_dif_mach < final_dif_mach;
+mach_bba.init_dif_rms = nanmean(init_dif_mach, 1);
+mach_bba.final_dif_rms = nanmean(final_dif_mach, 1);
+
     % goal_bba = offset_quad - offset_bpm;
     % goal_bba(offset_bba == 0) = NaN;
     % stat_bba.goal_bba = goal_bba;
