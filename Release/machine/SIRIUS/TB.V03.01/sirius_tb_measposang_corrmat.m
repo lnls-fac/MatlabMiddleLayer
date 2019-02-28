d5649754717f5bf0bb2dd1a7952bd3ef6648c68b
anacso17 <anacso1777@gmail.com>
2019-02-28 10:22:18 -0300

    Add minor changes to sirius_tb_measposang_corrmat

--- Release/machine/SIRIUS/TB.V03.01/sirius_tb_measposang_corrmat.m
+++ Release/machine/SIRIUS/TB.V03.01/sirius_tb_measposang_corrmat.m
@@ -1,15 +1,15 @@
-function sirius_tb_measposang_corrmat
+function [m_h, m_v] = sirius_tb_measposang_corrmat
 
 global THERING
 data = sirius_tb_family_data(THERING);
 chv = data.CHV.ATIndex;
 ch = {chv(end-1), data.InjSept.ATIndex};
 cv = {chv(end-1), chv(end)};
-i_init = ch{1};
-i_end = length(THERING);
 
 kick = 1e-5;
 
+i_init = ch{1};
+i_end = length(THERING);
 m_h = zeros(2,2);
 for i=1:length(ch)
     the_ring = lnls_set_kickangle(THERING, +kick/2, ch{i}, 'x');
@@ -19,6 +19,8 @@ for i=1:length(ch)
     m_h(:,i) = (p2([1,2]) - p1([1,2]))/kick;
 end
 
+i_init = cv{1};
+i_end = length(THERING);
 m_v = zeros(2,2);
 for i=1:length(cv)
     the_ring = lnls_set_kickangle(THERING, +kick/2, cv{i}, 'y');
