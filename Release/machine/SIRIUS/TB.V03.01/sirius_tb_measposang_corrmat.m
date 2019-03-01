fbc6b65168a370c3d0c63e53bbb027d590514126
anacso17 <anacso1777@gmail.com>
2019-03-01 15:14:58 -0300

    Fix measpos_corrmat scripts.

--- Release/machine/SIRIUS/TB.V03.01/sirius_tb_measposang_corrmat.m
+++ Release/machine/SIRIUS/TB.V03.01/sirius_tb_measposang_corrmat.m
@@ -1,7 +1,6 @@
-function [m_h, m_v] = sirius_tb_measposang_corrmat
+function [m_h, m_v, ch, cv] = sirius_tb_measposang_corrmat(thering)
 
-global THERING
-data = sirius_tb_family_data(THERING);
+data = sirius_tb_family_data(thering);
 chv = data.CHV.ATIndex;
 ch = {chv(end-1), data.InjSept.ATIndex};
 cv = {chv(end-1), chv(end)};
@@ -9,25 +8,25 @@ cv = {chv(end-1), chv(end)};
 kick = 1e-5;
 
 i_init = ch{1};
-i_end = length(THERING);
+i_end = length(thering);
 m_h = zeros(2,2);
 for i=1:length(ch)
-    the_ring = lnls_set_kickangle(THERING, +kick/2, ch{i}, 'x');
-    p1 = linepass(the_ring(i_init:i_end), [0,0,0,0,0,0]');
-    the_ring = lnls_set_kickangle(THERING, -kick/2, ch{i}, 'x');
-    p2 = linepass(the_ring(i_init:i_end), [0,0,0,0,0,0]');
-    m_h(:,i) = (p2([1,2]) - p1([1,2]))/kick;
+    thering = lnls_set_kickangle(thering, +kick/2, ch{i}, 'x');
+    p1 = linepass(thering(i_init:i_end), [0,0,0,0,0,0]');
+    thering = lnls_set_kickangle(thering, -kick/2, ch{i}, 'x');
+    p2 = linepass(thering(i_init:i_end), [0,0,0,0,0,0]');
+    m_h(:,i) = -(p2([1,2]) - p1([1,2]))/kick;
 end
 
 i_init = cv{1};
-i_end = length(THERING);
+i_end = length(thering);
 m_v = zeros(2,2);
 for i=1:length(cv)
-    the_ring = lnls_set_kickangle(THERING, +kick/2, cv{i}, 'y');
-    p1 = linepass(the_ring(i_init:i_end), [0,0,0,0,0,0]');
-    the_ring = lnls_set_kickangle(THERING, -kick/2, cv{i}, 'y');
-    p2 = linepass(the_ring(i_init:i_end), [0,0,0,0,0,0]');
-    m_v(:,i) = (p2([3,4]) - p1([3,4]))/kick;
+    thering = lnls_set_kickangle(thering, +kick/2, cv{i}, 'y');
+    p1 = linepass(thering(i_init:i_end), [0,0,0,0,0,0]');
+    thering = lnls_set_kickangle(thering, -kick/2, cv{i}, 'y');
+    p2 = linepass(thering(i_init:i_end), [0,0,0,0,0,0]');
+    m_v(:,i) = -(p2([3,4]) - p1([3,4]))/kick;
 end
 
 end
--- Release/machine/SIRIUS/TS.V03.04/sirius_ts_measposang_corrmat.m
+++ Release/machine/SIRIUS/TS.V03.04/sirius_ts_measposang_corrmat.m
@@ -1,33 +1,32 @@
-function [m_h, m_v] = sirius_ts_measposang_corrmat
+function [m_h, m_v, ch, cv] = sirius_ts_measposang_corrmat(thering)
 
-global THERING
-ch_index = findcells(THERING, 'FamName', 'CH');
-cv_index = findcells(THERING, 'FamName', 'CV');
-ch = {ch_index(end), findcells(THERING, 'FamName', 'InjSeptF')};
+ch_index = findcells(thering, 'FamName', 'CH');
+cv_index = findcells(thering, 'FamName', 'CV');
+ch = {ch_index(end), findcells(thering, 'FamName', 'InjSeptF')};
 cv = {cv_index(end-1), cv_index(end)};
 
 kick = 1e-5;
 
 i_init = ch{1};
-i_end = length(THERING);
+i_end = length(thering);
 m_h = zeros(2,2);
 for i=1:length(ch)
-    the_ring = lnls_set_kickangle(THERING, +kick/2, ch{i}, 'x');
+    the_ring = lnls_set_kickangle(thering, +kick/2, ch{i}, 'x');
     p1 = linepass(the_ring(i_init:i_end), [0,0,0,0,0,0]');
-    the_ring = lnls_set_kickangle(THERING, -kick/2, ch{i}, 'x');
+    the_ring = lnls_set_kickangle(thering, -kick/2, ch{i}, 'x');
     p2 = linepass(the_ring(i_init:i_end), [0,0,0,0,0,0]');
-    m_h(:,i) = (p2([1,2]) - p1([1,2]))/kick;
+    m_h(:,i) = -(p2([1,2]) - p1([1,2]))/kick;
 end
 
 i_init = cv{1};
-i_end = length(THERING);
+i_end = length(thering);
 m_v = zeros(2,2);
 for i=1:length(cv)
-    the_ring = lnls_set_kickangle(THERING, +kick/2, cv{i}, 'y');
+    the_ring = lnls_set_kickangle(thering, +kick/2, cv{i}, 'y');
     p1 = linepass(the_ring(i_init:i_end), [0,0,0,0,0,0]');
-    the_ring = lnls_set_kickangle(THERING, -kick/2, cv{i}, 'y');
+    the_ring = lnls_set_kickangle(thering, -kick/2, cv{i}, 'y');
     p2 = linepass(the_ring(i_init:i_end), [0,0,0,0,0,0]');
-    m_v(:,i) = (p2([3,4]) - p1([3,4]))/kick;
+    m_v(:,i) = -(p2([3,4]) - p1([3,4]))/kick;
 end
 
 end
