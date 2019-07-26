4fa9d3f20f4c6115fc4349c44a3cd4d08b357d33
ximenes <xresende@gmail.com>
2019-07-26 17:19:00 -0300

    Update TB.V03.02 strengths with fitted optics from experiment 2019-07-16

--- Release/machine/SIRIUS/TB.V03.02/set_parameters_tb.m
+++ Release/machine/SIRIUS/TB.V03.02/set_parameters_tb.m
@@ -1,35 +1,50 @@
 if strcmp(mode_version,'M1')
-    
+
     %%% Initial Conditions from Linac measured parameters
     %%% Linac second quadrupole triplet set to same values used during measurements
     IniCond.Dispersion = [0.0, 0.0, 0.0, 0.0]';
     IniCond.beta =  [ 1.52012, 4.41004]';
     IniCond.alpha = [ -1.16128, 1.09985]';
-    
+
     %%% Quadrupoles
-    qf2L_strength     =  12.37;
-    qd2L_strength     = -14.85;
-    qf3L_strength     =  2.574953879766;
+%     qf2L_strength     =  12.37;
+%     qd2L_strength     = -14.85;
+%     qf3L_strength     =  2.574953879766;
 
-    qd1_strength      = -8.822297193517;
-    qf1_strength      = 13.336070151902;
-    qd2a_strength     = -11.545107518252;
-    qf2a_strength     = 13.778320896856;
-    qf2b_strength     = 10.010343001131;
-    qd2b_strength     = -9.131081489222;
-    qf3_strength      = 9.853067340092;
-    qd3_strength      = -4.85435424189;
-    qf4_strength      = 11.764854917212;
-    qd4_strength      = -7.260998667337;
+%     qd1_strength      = -8.822297193517;
+%     qf1_strength      = 13.336070151902;
+%     qd2a_strength     = -11.545107518252;
+%     qf2a_strength     = 13.778320896856;
+%     qf2b_strength     = 10.010343001131;
+%     qd2b_strength     = -9.131081489222;
+%     qf3_strength      = 9.853067340092;
+%     qd3_strength      = -4.85435424189;
+%     qf4_strength      = 11.764854917212;
+%     qd4_strength      = -7.260998667337;
 
+    % 2019-07-25 propagated from LINAC measurements of 2019-07-16
+    qf2L_strength     =  11.79;
+    qd2L_strength     = -14.30;
+    qf3L_strength     =  4.80;
+
+    qd1_strength      = -8.82;
+    qf1_strength      = 13.34;
+    qd2a_strength     = -9.38;
+    qf2a_strength     = 12.67;
+    qf2b_strength     =  7.99;
+    qd2b_strength     = -7.12;
+    qf3_strength      = 10.33;
+    qd3_strength      = -5.52;
+    qf4_strength      = 11.64;
+    qd4_strength      = -6.94;
 
 elseif strcmp(mode_version,'M2')
-    
+
     %%% Initial Conditions
     IniCond.Dispersion = [0.0, 0.0, 0.0, 0.0]';
     IniCond.beta =  [ 10.0, 10.0]';
     IniCond.alpha = [ 0.0, 0.0]';
-    
+
     %%% Quadrupoles
     qf2L_strength     = 4.18478853946 ;
     qd2L_strength     = -3.771441137844;
@@ -45,14 +60,16 @@ elseif strcmp(mode_version,'M2')
     qd3_strength      = -5.708542016046;
     qf4_strength      = 11.838583437399;
     qd4_strength      = -7.274915332856;
-    
+
+
+
 elseif strcmp(mode_version,'M3')
-    
+
     %%% Initial Conditions
     IniCond.Dispersion = [0.0, 0.0, 0.0, 0.0]';
     IniCond.beta =  [ 7.0, 7.0]';
     IniCond.alpha = [ -1.0, -1.0]';
-    
+
     %%% Quadrupoles
     qf2L_strength     =  4.041196327091;
     qd2L_strength     = -0.650383961873;
@@ -68,14 +85,14 @@ elseif strcmp(mode_version,'M3')
     qd3_strength      = -5.991431546489;
     qf4_strength      = 11.854633649452;
     qd4_strength      = -7.265597342644;
- 
+
 elseif strcmp(mode_version,'M4')
-    
+
     %%% Initial Conditions
     IniCond.Dispersion = [0.0, 0.0, 0.0, 0.0]';
     IniCond.beta =  [ 7.0, 7.0]';
     IniCond.alpha = [ 1.0, 1.0]';
-    
+
     %%% Quadrupoles
     qf2L_strength     =  3.31553030565;
     qd2L_strength     = -3.298430735342;
@@ -91,14 +108,14 @@ elseif strcmp(mode_version,'M4')
     qd3_strength      = -5.361705635217;
     qf4_strength      = 11.408003594895;
     qd4_strength      = -6.555793655328;
-    
+
 elseif strcmp(mode_version,'M5')
-    
+
     %%% Initial Conditions
     IniCond.Dispersion = [0.0, 0.0, 0.0, 0.0]';
     IniCond.beta =  [ 7.0, 7.0]';
     IniCond.alpha = [ 1.0, -1.0]';
-    
+
     %%% Quadrupoles
     qf2L_strength     = 3.847021491694;
     qd2L_strength     = -4.051995180352;
@@ -116,12 +133,12 @@ elseif strcmp(mode_version,'M5')
     qd4_strength      =  -6.835374207679;
 
 elseif strcmp(mode_version,'M6')
-    
+
     %%% Initial Conditions
     IniCond.Dispersion = [0.0, 0.0, 0.0, 0.0]';
     IniCond.beta =  [ 7.0, 7.0]';
     IniCond.alpha = [ -1.0, 1.0]';
-    
+
     %%% Quadrupoles
     qf2L_strength     = 4.782319763405;
     qd2L_strength     = -3.290324589514;
@@ -137,7 +154,7 @@ elseif strcmp(mode_version,'M6')
     qd3_strength      = -5.325646205442;
     qf4_strength      = 11.806095745404;
     qd4_strength      = -7.269145836899;
-    
+
 else
     error('caso nao implementado');
 end
