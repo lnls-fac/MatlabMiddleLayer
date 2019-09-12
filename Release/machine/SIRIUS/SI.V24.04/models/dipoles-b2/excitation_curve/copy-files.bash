a1451fd085d672fc03093565d6379025a2a6248b
murilobalves <alvesb.murilo@gmail.com>
2019-09-12 11:58:52 -0300

    Move and rename excitation curve data files for SI-B1 and SI-B2

---
+++ Release/machine/SIRIUS/SI.V24.04/models/dipoles-b2/excitation_curve/copy-files.bash
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+function copy_files {
+  current=$1
+  side=$2
+  cp /home/imas/repos/si-dipoles-b2/model-08/analysis/hallprobe/production/x0-8p153mm-reftraj/B2-001/$current/z-$side/analysis.txt ./b2-001-$current-$side.txt
+  cp /home/imas/repos/si-dipoles-b2/model-08/analysis/hallprobe/production/x0-8p153mm-reftraj/B2-002/$current/z-$side/analysis.txt ./b2-002-$current-$side.txt
+  cp /home/imas/repos/si-dipoles-b2/model-08/analysis/hallprobe/production/x0-8p153mm-reftraj/B2-003/$current/z-$side/analysis.txt ./b2-003-$current-$side.txt
+  cp /home/imas/repos/si-dipoles-b2/model-08/analysis/hallprobe/production/x0-8p153mm-reftraj/B2-004/$current/z-$side/analysis.txt ./b2-004-$current-$side.txt
+  cp /home/imas/repos/si-dipoles-b2/model-08/analysis/hallprobe/production/x0-8p153mm-reftraj/B2-005/$current/z-$side/analysis.txt ./b2-005-$current-$side.txt
+  cp /home/imas/repos/si-dipoles-b2/model-08/analysis/hallprobe/production/x0-8p153mm-reftraj/B2-006/$current/z-$side/analysis.txt ./b2-006-$current-$side.txt
+  cp /home/imas/repos/si-dipoles-b2/model-08/analysis/hallprobe/production/x0-8p153mm-reftraj/B2-007/$current/z-$side/analysis.txt ./b2-007-$current-$side.txt
+  cp /home/imas/repos/si-dipoles-b2/model-08/analysis/hallprobe/production/x0-8p153mm-reftraj/B2-008/$current/z-$side/analysis.txt ./b2-008-$current-$side.txt
+  cp /home/imas/repos/si-dipoles-b2/model-08/analysis/hallprobe/production/x0-8p153mm-reftraj/B2-009/$current/z-$side/analysis.txt ./b2-009-$current-$side.txt
+  cp /home/imas/repos/si-dipoles-b2/model-08/analysis/hallprobe/production/x0-8p153mm-reftraj/B2-010/$current/z-$side/analysis.txt ./b2-010-$current-$side.txt
+  cp /home/imas/repos/si-dipoles-b2/model-08/analysis/hallprobe/production/x0-8p153mm-reftraj/B2-011/$current/z-$side/analysis.txt ./b2-011-$current-$side.txt
+  cp /home/imas/repos/si-dipoles-b2/model-08/analysis/hallprobe/production/x0-8p153mm-reftraj/B2-013/$current/z-$side/analysis.txt ./b2-013-$current-$side.txt
+  cp /home/imas/repos/si-dipoles-b2/model-08/analysis/hallprobe/production/x0-8p153mm-reftraj/B2-014/$current/z-$side/analysis.txt ./b2-014-$current-$side.txt
+  cp /home/imas/repos/si-dipoles-b2/model-08/analysis/hallprobe/production/x0-8p153mm-reftraj/B2-015/$current/z-$side/analysis.txt ./b2-015-$current-$side.txt
+  cp /home/imas/repos/si-dipoles-b2/model-08/analysis/hallprobe/production/x0-8p153mm-reftraj/B2-016/$current/z-$side/analysis.txt ./b2-016-$current-$side.txt
+  cp /home/imas/repos/si-dipoles-b2/model-08/analysis/hallprobe/production/x0-8p153mm-reftraj/B2-017/$current/z-$side/analysis.txt ./b2-017-$current-$side.txt
+  cp /home/imas/repos/si-dipoles-b2/model-08/analysis/hallprobe/production/x0-8p153mm-reftraj/B2-018/$current/z-$side/analysis.txt ./b2-018-$current-$side.txt
+  cp /home/imas/repos/si-dipoles-b2/model-08/analysis/hallprobe/production/x0-8p153mm-reftraj/B2-019/$current/z-$side/analysis.txt ./b2-019-$current-$side.txt
+  cp /home/imas/repos/si-dipoles-b2/model-08/analysis/hallprobe/production/x0-8p153mm-reftraj/B2-021/$current/z-$side/analysis.txt ./b2-021-$current-$side.txt
+  cp /home/imas/repos/si-dipoles-b2/model-08/analysis/hallprobe/production/x0-8p153mm-reftraj/B2-022/$current/z-$side/analysis.txt ./b2-022-$current-$side.txt
+  cp /home/imas/repos/si-dipoles-b2/model-08/analysis/hallprobe/production/x0-8p153mm-reftraj/B2-023/$current/z-$side/analysis.txt ./b2-023-$current-$side.txt
+  cp /home/imas/repos/si-dipoles-b2/model-08/analysis/hallprobe/production/x0-8p153mm-reftraj/B2-024/$current/z-$side/analysis.txt ./b2-024-$current-$side.txt
+  cp /home/imas/repos/si-dipoles-b2/model-08/analysis/hallprobe/production/x0-8p153mm-reftraj/B2-025/$current/z-$side/analysis.txt ./b2-025-$current-$side.txt
+  cp /home/imas/repos/si-dipoles-b2/model-08/analysis/hallprobe/production/x0-8p153mm-reftraj/B2-026/$current/z-$side/analysis.txt ./b2-026-$current-$side.txt
+  cp /home/imas/repos/si-dipoles-b2/model-08/analysis/hallprobe/production/x0-8p153mm-reftraj/B2-027/$current/z-$side/analysis.txt ./b2-027-$current-$side.txt
+  cp /home/imas/repos/si-dipoles-b2/model-08/analysis/hallprobe/production/x0-8p153mm-reftraj/B2-028/$current/z-$side/analysis.txt ./b2-028-$current-$side.txt
+  cp /home/imas/repos/si-dipoles-b2/model-08/analysis/hallprobe/production/x0-8p153mm-reftraj/B2-029/$current/z-$side/analysis.txt ./b2-029-$current-$side.txt
+  cp /home/imas/repos/si-dipoles-b2/model-08/analysis/hallprobe/production/x0-8p153mm-reftraj/B2-030/$current/z-$side/analysis.txt ./b2-030-$current-$side.txt
+  cp /home/imas/repos/si-dipoles-b2/model-08/analysis/hallprobe/production/x0-8p153mm-reftraj/B2-031/$current/z-$side/analysis.txt ./b2-031-$current-$side.txt
+  cp /home/imas/repos/si-dipoles-b2/model-08/analysis/hallprobe/production/x0-8p153mm-reftraj/B2-032/$current/z-$side/analysis.txt ./b2-032-$current-$side.txt
+  cp /home/imas/repos/si-dipoles-b2/model-08/analysis/hallprobe/production/x0-8p153mm-reftraj/B2-033/$current/z-$side/analysis.txt ./b2-033-$current-$side.txt
+  cp /home/imas/repos/si-dipoles-b2/model-08/analysis/hallprobe/production/x0-8p153mm-reftraj/B2-034/$current/z-$side/analysis.txt ./b2-034-$current-$side.txt
+  cp /home/imas/repos/si-dipoles-b2/model-08/analysis/hallprobe/production/x0-8p153mm-reftraj/B2-036/$current/z-$side/analysis.txt ./b2-036-$current-$side.txt
+  cp /home/imas/repos/si-dipoles-b2/model-08/analysis/hallprobe/production/x0-8p153mm-reftraj/B2-037/$current/z-$side/analysis.txt ./b2-037-$current-$side.txt
+  cp /home/imas/repos/si-dipoles-b2/model-08/analysis/hallprobe/production/x0-8p153mm-reftraj/B2-038/$current/z-$side/analysis.txt ./b2-038-$current-$side.txt
+  cp /home/imas/repos/si-dipoles-b2/model-08/analysis/hallprobe/production/x0-8p153mm-reftraj/B2-040/$current/z-$side/analysis.txt ./b2-040-$current-$side.txt
+  cp /home/imas/repos/si-dipoles-b2/model-08/analysis/hallprobe/production/x0-8p153mm-reftraj/B2-042/$current/z-$side/analysis.txt ./b2-042-$current-$side.txt
+  cp /home/imas/repos/si-dipoles-b2/model-08/analysis/hallprobe/production/x0-8p153mm-reftraj/B2-043/$current/z-$side/analysis.txt ./b2-043-$current-$side.txt
+  cp /home/imas/repos/si-dipoles-b2/model-08/analysis/hallprobe/production/x0-8p153mm-reftraj/B2-044/$current/z-$side/analysis.txt ./b2-044-$current-$side.txt
+  cp /home/imas/repos/si-dipoles-b2/model-08/analysis/hallprobe/production/x0-8p153mm-reftraj/B2-045/$current/z-$side/analysis.txt ./b2-045-$current-$side.txt
+  cp /home/imas/repos/si-dipoles-b2/model-08/analysis/hallprobe/production/x0-8p153mm-reftraj/B2-046/$current/z-$side/analysis.txt ./b2-046-$current-$side.txt
+}
+
+copy_files 381p7A positive
+copy_files 401p8A positive
+copy_files 421p9A positive
+copy_files 381p7A negative
+copy_files 401p8A negative
+copy_files 421p9A negative
